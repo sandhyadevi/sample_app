f7c273abb2ccf17143cb2104a8ed2d644b2b76a6
sandhyadevi <sdevis09@gmail.com>
2013-06-27 10:21:01 +0530

    Finish sign in

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -24,6 +24,14 @@ describe "User pages" do
         expect { click_button submit }.not_to change(User, :count)
       end
     end
+	
+	describe "after submission" do
+        before { click_button submit }
+
+        it { should have_selector('title', text: 'Sign up') }
+        it { should have_content('error') }
+      end
+
 
     describe "with valid information" do
       before do
@@ -33,9 +41,22 @@ describe "User pages" do
         fill_in "Confirmation", with: "foobar"
       end
 
+	        describe "after saving the user" do
+        before { click_button submit }
+        let(:user) { User.find_by_email('user@example.com') }
+
+        it { should have_selector('title', text: user.name) }
+        it { should have_selector('div.alert.alert-success', text: 'Welcome') }
+      end
+       it { should have_link('Sign out') }
       it "should create a user" do
         expect { click_button submit }.to change(User, :count).by(1)
       end
+	        describe "followed by signout" do
+        before { click_link "Sign out" }
+        it { should have_link('Sign in') }
+      end
+
     end
   end
 
