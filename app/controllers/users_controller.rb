a92bbbcfaae7e1f7ee81119345c2e079a153abe6
sandhyadevi <sdevis09@gmail.com>
2013-06-28 09:48:17 +0530

    Add user following

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,27 +1,23 @@
 class UsersController < ApplicationController
-  before_filter :signed_in_user, only: [:index, :edit, :update]
+  before_filter :signed_in_user, 
+                only: [:index, :edit, :update, :destroy, :following, :followers]
   before_filter :correct_user,   only: [:edit, :update]
   before_filter :admin_user,     only: :destroy
 
+  def index
+    @users = User.paginate(page: params[:page])
+  end
 
-    def show
+  def show
     @user = User.find(params[:id])
     @microposts = @user.microposts.paginate(page: params[:page])
   end
 
-
-   def new
+  def new
     @user = User.new
   end
-  
-      def index
-    @users = User.paginate(page: params[:page])
-  end
 
-
-
-
-      def create
+  def create
     @user = User.new(params[:user])
     if @user.save
       sign_in @user
@@ -33,8 +29,8 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(params[:id])
   end
+
   def update
     if @user.update_attributes(params[:user])
       flash[:success] = "Profile updated"
@@ -44,21 +40,28 @@ class UsersController < ApplicationController
       render 'edit'
     end
   end
+
   def destroy
     User.find(params[:id]).destroy
     flash[:success] = "User destroyed."
-    redirect_to users_url
+    redirect_to users_path
   end
 
+  def following
+    @title = "Following"
+    @user = User.find(params[:id])
+    @users = @user.followed_users.paginate(page: params[:page])
+    render 'show_follow'
+  end
 
-    private
+  def followers
+    @title = "Followers"
+    @user = User.find(params[:id])
+    @users = @user.followers.paginate(page: params[:page])
+    render 'show_follow'
+  end
 
-    def signed_in_user
-      unless signed_in?
-        store_location
-        redirect_to signin_url, notice: "Please sign in."
-      end
-    end
+  private
 
     def correct_user
       @user = User.find(params[:id])
@@ -68,7 +71,4 @@ class UsersController < ApplicationController
     def admin_user
       redirect_to(root_path) unless current_user.admin?
     end
-
-
-
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,23 @@
 SampleApp::Application.routes.draw do
-resources :users
-   resources :sessions, only: [:new, :create, :destroy]
-  resources :microposts, only: [:create, :destroy]
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
+  resources :sessions,      only: [:new, :create, :destroy]
+  resources :microposts,    only: [:create, :destroy]
+  resources :relationships, only: [:create, :destroy]
+  
   root to: 'static_pages#home'
-  match '/signup',  to: 'users#new'
 
-  match '/help',    to: 'static_pages#help'
-  match '/about',   to: 'static_pages#about'
-  match '/contact', to: 'static_pages#contact'
-   match '/signin',  to: 'sessions#new'
+  match '/signup',  to:'users#new'
+  match '/signin',  to:'sessions#new'
   match '/signout', to: 'sessions#destroy', via: :delete
 
-
-
+  match '/help',  to: 'static_pages#help'
+  match '/about',  to: 'static_pages#about'
+  match '/contact',  to: 'static_pages#contact'
+  
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -62,7 +67,7 @@ resources :users
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-   root :to => 'welcome#index'
+  # root :to => 'welcome#index'
 
   # See how all your routes lay out with "rake routes"
 
